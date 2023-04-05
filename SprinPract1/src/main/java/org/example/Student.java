e288bc4744afc28bb8abaf114a009042e55b1dbc
WaleedQureshi141 <129791489+WaleedQureshi141@users.noreply.github.com>
2023-04-05 12:25:57 -0400

    Add files via upload

---
+++ SprinPract1/src/main/java/org/example/Student.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class Student
+{
+    private int id;
+    private String name;
+
+    private List<Phone> ph;
+    private Address add;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPh(List<Phone> ph) {
+        this.ph = ph;
+    }
+
+    public void setAdd(Address add) {
+        this.add = add;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", \n ph=" + ph.toString() +
+                ", \n add=" + add.toString() +
+                '}';
+    }
+}
